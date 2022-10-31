bf5a801f63a5e6bf79879311c46026213edf4967
zeredogo <zeredogo@gmail.com>
2022-10-31 23:23:56 +0100

    update made

--- market.sol
+++ market.sol
@@ -5,9 +5,12 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Market {
     
     struct Product {
+        address payable owner;
         string name;
         string image;
         string description;
-        
+        string location;
+        uint price;
+        uint sold;
     }
 }
