1051236bc6a5436efef31c20551d4b496a7279d9
zeredogo <zeredogo@gmail.com>
2022-10-31 23:11:46 +0100

    update made

---
+++ market.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Market {
+    
+    struct Product {
+        string name;
+        string image;
+        string description;
+        
+    }
+}
