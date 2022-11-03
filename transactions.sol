0cf263be3e8430adae3d5da3b483ebedda45a231
zeredogo <zeredogo@gmail.com>
2022-11-03 02:04:17 +0100

    update made

--- transactions.sol
+++ transactions.sol
@@ -8,4 +8,6 @@ interface IERC20Token {
     function transferForm(address, address, uint256) external returns (bool);
     function totalSupply() external view returns (uint256);
     function balanceOf(address) external view returns (uint256);
+    function allowance(address, address) external view returns (uint256);
+    
 }
