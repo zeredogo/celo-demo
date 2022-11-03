ca5a3554edd0f34fdb933f9af04a0615762d917f
zeredogo <zeredogo@gmail.com>
2022-11-02 19:54:57 -0500

    update made

--- transactions.sol
+++ transactions.sol
@@ -4,5 +4,6 @@ pragma solidity >=0.7.0 <0.9.0;
 
 interface IERC20Token {
     function transfer(address, uint256) external returns (bool);
-    
+    function approve(address, uint256) external returns (bool);
+    function transferForm(address, address, uint256) external returns (bool);
 }
