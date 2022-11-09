0baa27dff50d610fac07ac9584cc95f5a871f397
zeredogo <zeredogo@gmail.com>
2022-11-09 07:14:16 +0100

    changes made

--- demo/demo.sol
+++ demo/demo.sol
@@ -5,5 +5,9 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Demo {
     uint internal productsLength = 0;
 
-    
+     struct Product {
+        address payable owner;
+        string name;
+        
+    }
 }
