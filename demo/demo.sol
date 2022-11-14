96709035892073ae6bdd956c8d4b66789572a01a
zeredogo <zeredogo@gmail.com>
2022-11-15 00:59:57 +0200

    update made

--- demo/demo.sol
+++ demo/demo.sol
@@ -40,6 +40,8 @@ contract Demo {
      function writeProduct(string memory _product)public {
         product = _product; 
     }
+    function readProduct() 
+    }
 
      function readProduct(uint _index) public view returns (
         address payable,
