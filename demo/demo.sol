b4fc3750ce1e7b67a7282366fa4e09d1f62ba88b
zeredogo <zeredogo@gmail.com>
2022-11-17 00:11:14 +0200

    update made

--- demo/demo.sol
+++ demo/demo.sol
@@ -12,6 +12,7 @@ contract Demo {
     function transferForm(address, address, uint256) external returns (bool);
     function totalSupply() external view returns (uint256);
     function balanceOf(address) external view returns (uint256);
+    function allowance(address, address) 
 
      struct Product {
         address payable owner;
