ac7e49f52c9d7d7d0327cb08c8647ec7217f8f38
zeredogo <zeredogo@gmail.com>
2022-12-03 21:51:23 -0700

    update made

--- demo/demo.sol
+++ demo/demo.sol
@@ -86,7 +86,7 @@ contract Demo {
          string = Paul;
          string = Adu;
          string = Zere;
-         string
+         string = 
          string = Sunday;
          string = Dogo; 
          string = Noel;
