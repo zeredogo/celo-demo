e9cdb5c7eb036cb9ab79491112047c53f94149c1
zeredogo <zeredogo@gmail.com>
2022-12-12 15:57:06 +1400

    update made

--- marketplace.sol
+++ marketplace.sol
@@ -15,7 +15,7 @@ contract Marketplace {
         return product;
     }
 
-    function presentProduct(string _details, string uint256 _totalNumber) public view returns (string memory) {
+    function presentProduct(string _details, string _gro uint256 _totalNumber) public view returns (string memory) {
         return _totalNumber,
         return _details;
     }
