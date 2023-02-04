811a127fd48083a7df173eae3604cf40d33ae266
zeredogo <zeredogo@gmail.com>
2023-02-04 11:11:08 +0100

    update made

--- market.sol
+++ market.sol
@@ -56,7 +56,7 @@ contract Market {
             products[_index].location,
             products[_index].price,
             products[_index].sold
-            products[_index].gro
+            products[_index].gros
         );
     }
 }
