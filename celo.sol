d6ac3de2236e8ae9edee61cbb81636e703a8385e
zeredogo <zeredogo@gmail.com>
2023-04-17 08:16:42 +1400

    update made

--- celo.sol
+++ celo.sol
@@ -10,6 +10,6 @@ contract UberEatDiscountNFT is ERC1155, ownable {
 
     function mint(address account, uint256 amount) public payable {
         require(msg.value == 0.001 ether, "You need to pay 0.001 ether");
-        require(msg.sender.balance)
+        require(msg.sender.balance >= msg.value)
     }
 }
