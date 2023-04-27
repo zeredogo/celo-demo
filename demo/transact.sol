72340f10e84f828091fea13842385cdd95b7e7f0
zeredogo <zeredogo@gmail.com>
2023-04-26 23:36:52 -1200

    update made

--- demo/transact.sol
+++ demo/transact.sol
@@ -4,3 +4,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyNFT is ERC721, Ownable {
+    uint public constant maxNFTs = 20;
+    uint public nftCount = 0;
+    uint public price = 0.1 ether;
+    string private _baseTokenURI;
