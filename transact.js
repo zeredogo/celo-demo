3f49ac06f4eb24a35151bfb5bafe91b187f79b2a
zeredogo <zeredogo@gmail.com>
2023-04-28 01:47:39 +1400

    update made

--- transact.js
+++ transact.js
@@ -3,3 +3,4 @@ const contractABI = require('./build/contracts/MyNFT.json').abi
 
 const web3 = new Web3('https://alfajores-forno.celo-testnet.org')
 const contractAddress = '0x1234567890123456789012345678901234567890'
+const contractInstance = new web3.eth.Contract(contractABI, contractAddress)
