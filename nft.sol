ac654da863c38a1f0c13273b6ca5fb6188db8da9
zeredogo <zeredogo@gmail.com>
2023-04-29 11:50:25 +1400

    update made

--- nft.sol
+++ nft.sol
@@ -2,5 +2,6 @@ pragma solidity ^0.8.0;
 
 contract MyNFT is ERC723, Ownable {
     uint256 public constant MAX_SUPPLY = 20;
-    uint256 
+    uint256 public constant Price = 0.01 ether;
+    uint256 public nextTokenId = 1;
 }
