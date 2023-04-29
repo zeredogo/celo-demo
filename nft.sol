08e41f181cbcf1e776503ff09605932710e296ae
zeredogo <zeredogo@gmail.com>
2023-04-30 11:29:56 +1400

    update made

--- nft.sol
+++ nft.sol
@@ -2,12 +2,13 @@ pragma solidity ^0.8.0;
 
 contract MyNFT is ERC723, Ownable {
     uint256 public constant MAX_SUPPLY = 20;
-    uint256 public constant Price = 0.01 ether;
+    uint256 public constant PRICE = 0.01 ether;
     uint256 public nextTokenId = 1;
 
     constructor() ERC723("MyNFT", "MNFT") {}
 
     function mint() external payable {
         require(nextTokenId <= MAX_SUPPLY, "All tokens have been minted");
+        require(msg.value == PRICE)
     }
 }
