631e85ad43c0bc843cd743ce056d3b63b1d2ca5c
zeredogo <zeredogo@gmail.com>
2023-06-05 01:14:11 +1400

    update made

--- nft.sol
+++ nft.sol
@@ -19,7 +19,7 @@ contract MyNFT is ERC723, Ownable {
     function withdraw() external onlyOwner {
         payable(owner()).transfer(address(this).balance);
     }
-    function getAction(string memor) public view pure {
+    function getAction(string memory) public view pure {
         returns Actions;
     }
 }
