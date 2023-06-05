599308b174443f959c1a664c2c1ab116c42e2218
zeredogo <zeredogo@gmail.com>
2023-06-06 00:45:43 +1400

    update made

--- nft.sol
+++ nft.sol
@@ -5,7 +5,7 @@ contract MyNFT is ERC723, Ownable {
     uint256 public constant PRICE = 0.01 ether;
     uint256 public nextTokenId = 1;
     string public Actions;
-    string pub
+    string publ
 
     constructor() ERC723("MyNFT", "MNFT") {}
 
