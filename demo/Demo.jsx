b992594c5058d50c6d4bd068f769cbf12dddcfb0
zeredogo <zeredogo@gmail.com>
2023-07-23 12:21:55 -1200

    update made

--- demo/Demo.jsx
+++ demo/Demo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Demo = () => {
   return (
     <div>
-        <h1>What is on my mind  and how  to deal </h1>
+        <h1>What is on my mind  and how  to deal w</h1>
     </div>
   )
 }
