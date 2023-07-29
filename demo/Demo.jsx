ce9eb7b854ccc73022301465a4232e9c4fad3746
zeredogo <zeredogo@gmail.com>
2023-07-28 16:43:32 -1200

    update made

--- demo/Demo.jsx
+++ demo/Demo.jsx
@@ -5,7 +5,9 @@ export const Demo = () => {
     <div>
         <h1>What is on my mind and how  
             to deal with this problem</h1>
-            <p>This is the exploration and the summition expi</p>
+            <p>This is the exploration and the summition expirience \
+                a
+            </p>
     </div>
   )
 }
