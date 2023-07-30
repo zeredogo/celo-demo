63e20b0b92585530d601a2cbbc7a9b1fecc7f2f5
zeredogo <zeredogo@gmail.com>
2023-07-29 16:43:53 -1200

    update made

--- demo/Demo.jsx
+++ demo/Demo.jsx
@@ -6,7 +6,7 @@ export const Demo = () => {
         <h1>What is on my mind and how  
             to deal with this problem</h1>
             <p>This is the exploration and the summition expirience \
-                a
+                all 
             </p>
     </div>
   )
