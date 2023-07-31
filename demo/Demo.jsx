faa6de5a11e7fa390a68310272a20a7c19f8a930
zeredogo <zeredogo@gmail.com>
2023-07-30 16:45:42 -1200

    update made

--- demo/Demo.jsx
+++ demo/Demo.jsx
@@ -6,7 +6,7 @@ export const Demo = () => {
         <h1>What is on my mind and how  
             to deal with this problem</h1>
             <p>This is the exploration and the summition expirience \
-                all my life suddently in charge, 
+                all my life suddently in charge, never
             </p>
     </div>
   )
