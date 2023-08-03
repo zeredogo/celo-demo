d457c1f3ca2b946e553b4b4e5a1f5e54e13e008d
zeredogo <zeredogo@gmail.com>
2023-08-03 05:57:43 +0100

    update made

--- demo/Demo.jsx
+++ demo/Demo.jsx
@@ -8,7 +8,7 @@ export const Demo = () => {
             <p>This is the exploration and the summition expirience \
                 all my life suddently in charge, never buga <br/>
                 in the rain beauty, Elon Musk bus stop aquafina money
-                over love big 7, kante everyday away DND
+                over love big 7, kante everyday away DND NSV
             </p>
     </div>
   )
