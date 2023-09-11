6990e2a3088d13742ff5a78d0c61d31cc5606216
zeredogo <zeredogo@gmail.com>
2023-09-11 23:34:02 +0100

    update commit

--- demo/Tran.sol
+++ demo/Tran.sol
@@ -3,6 +3,6 @@
 pragma solidity ^0.8.0;
 
 contract Tran {
-    string public name = 'Zonex, Kypto-zone, dogoempire = to Owner i';
+    string public name = 'Zonex, Kypto-zone, dogoempire = to Owner is';
     uint256 public Age = 28790;
 }
