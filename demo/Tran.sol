b20d74eaedfb33be7431280faf948a0b49b7ae64
zeredogo <zeredogo@gmail.com>
2023-11-07 07:43:13 +0100

    update commit

--- demo/Tran.sol
+++ demo/Tran.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 contract Tran {
     string public Logo = 'Zonex, Kypto-zone, dogoempire = Paul Zere Sunday Dogo.';
     uint256 public Age = 28790;
+    u
     string public Gracefullness = "The flood when and till flesh";
 
     function getLogo(string memory _logo) public view returns {
