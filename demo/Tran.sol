3c6a5122956493e19a1d3dcc32b2628c7a0dbb7f
zeredogo <zeredogo@gmail.com>
2023-11-26 21:02:46 +0100

    update commit

--- demo/Tran.sol
+++ demo/Tran.sol
@@ -12,5 +12,5 @@ contract Tran {
         return _logo;
     }
 
-    function writeGracefullness(string memory _gracefullness) publi
+    function writeGracefullness(string memory _gracefullness) public
 }
