ed051804fef0629e77dd4eef8674dcba94503ea4
zeredogo <zeredogo@gmail.com>
2024-02-14 20:38:34 +0100

    update commit

--- demo/Tran.sol
+++ demo/Tran.sol
@@ -22,7 +22,7 @@ contract Tran {
     }
     
     function getStart(string _start) public view returns {
-        return
+        return _
     }
 
     function getAge(uint256 _age) public view returns {
